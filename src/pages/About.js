9ec6446e854e2dcdbd972567054cdef0d086a162
Jas <c.jasminher@gmail.com>
2019-07-29 17:29:48 -0400

    Made navbar fixed, and styled home page, added icons to the about page, and also styled them

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,38 +1,62 @@
 import React from 'react';
-import { Col, Row, Container} from 'reactstrap'
+import { Col, Row, Container } from 'reactstrap'
 import '../css/About.css'
 
 function About() {
-    return (
-        <Container>
+  return (
+    <Container className="about-container">
 
 
-          <Row>
-            why choose see me
-          </Row>
+      <Row>
+        <Col lg="12">
+
+          <h1>Why choose SeeMe?</h1>
+          <p>Leading Industry in helping others when in crisis</p>
+        </Col>
+      </Row>
 
 
-          <Row>
-            <Col lg="3">
-            perfect matc
-            </Col>
+      <Row>
+        <Col lg="3">
+          <div className="icon">
+            <i className="fas fa-user-friends fa-3x" ></i>
+          </div>
 
-            <Col lg="3">
-            recognized
-            </Col>
+          <h5> Perfect Matching </h5>
+          <p> Our team are experts in matching you with the right person.</p>
+        </Col>
 
-            <Col lg="3">
-            fast matc
-            </Col>
+        <Col lg="3">
+          <div className="icon">
+            <i className="far fa-handshake fa-3x"></i>
+          </div>
 
-            <Col lg="3">
-            support 24/7
-            </Col>
-          </Row>
+          <h5> Recognised For Excellence </h5>
+          <p> We've been awarded for our high rate of customer satisfaction.</p>
+        </Col>
 
-        </Container>
+        <Col lg="3">
+          <div className="icon">
+            <i class="far fa-clock fa-3x"></i>
+          </div>
 
-    );
+          <h5> Fast Matching </h5>
+          <p> Almost immidiate matches with therapists and sponsors.</p>
+        </Col>
+
+        <Col lg="3">
+          <div className="icon">
+          <i class="fas fa-globe fa-3x"></i>
+          </div>
+
+          <h5> Support 24/7 </h5>
+          <p> We are avaiable all day, for us there is no such thing as a break. </p>
+        </Col>
+      </Row>
+
+    </Container>
+
+  );
 }
 
 export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import '../css/Home.css'
 import About from './About';
-
 import { Col } from 'reactstrap'
-
-
 
 
 
@@ -13,26 +10,21 @@ class Home extends React.Component {
     render() {
         return (
 
-
-
             <div className="main-container">
-
-
                 <div id="home-container">
 
+                    <p className="home-title"> Welcome to SeeMe </p>
 
-                        <p className="home-title"> Welcome to SeeMe </p>
 
+                    <Col lg={{ size: 6, offset: 3 }} md={{ size: 8, offset: 2 }}>
+                        <p className="home-info">
+                            A place where you can connect to therapists and sponsors whenever in crisis. 24/7 you'll be able to contact and meet up with them.
+                        </p>
+                    </Col>
 
-                        <Col lg={{ size: 6, offset: 3 }} md={{ size: 8, offset: 2 }}>
-                            <p className="home-info">
-                                A place where you can connect to therapists and sponsors whenever in crisis. 24/7 you'll be able to contact and meet up with them.
-                            </p>
-
-                        </Col>
                 </div>
 
-                <div name="about-container" id="about-container">
+                <div name="about-container">
                     <About />
                 </div>
 
