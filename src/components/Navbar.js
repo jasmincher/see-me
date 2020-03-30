18c1c62a571581b5cdfc970fd34167e7003fe8ce
Jas <c.jasminher@gmail.com>
2020-03-30 19:52:17 -0400

    style: Changed color of navbar when approaching about section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,105 +1,118 @@
 import {
-    Collapse,
-    Navbar as Navigation,
-    NavbarToggler,
-    Nav,
-    NavItem,
-    NavLink,
-    NavbarBrand
-} from 'reactstrap';
+  Collapse,
+  Navbar as Navigation,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarBrand
+} from "reactstrap";
 
-import '../css/Navbar.css'
-import React from 'react';
-import { NavLink as Link } from 'react-router-dom';
-import { scroller} from 'react-scroll'
+import "../css/Navbar.css";
+import React from "react";
+import { NavLink as Link } from "react-router-dom";
+import { scroller } from "react-scroll";
 
-import { withRouter } from 'react-router'
-
+import { withRouter } from "react-router";
 
 class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+      color: "transparent"
+    };
+  }
 
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-            isOpen: false
-        };
-    }
+  componentDidMount() {
+      //will change color of navbar, at home page, when it approaches about section
+    document.addEventListener("scroll", () => {
+      const backgroundcolor = window.scrollY >= 600 ? "black" : "transparent";
+      this.setState({ color: backgroundcolor });
+    });
+  }
 
-    toggle() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
 
+  scrollToAbout() {
+    scroller.scrollTo("about-container", {
+      duration: 1000,
+      delay: 100,
+      smooth: "easeInOutQuart"
+    });
+  }
 
-    scrollToAbout() {
-        scroller.scrollTo('about-container', {
-          duration: 1000,
-          delay: 100,
-          smooth: 'easeInOutQuart'
-        })
-      }
+  scrollToHome() {
+    scroller.scrollTo("home-container", {
+      duration: 1000,
+      delay: 100,
+      smooth: "easeInOutQuart"
+    });
+  }
 
-      scrollToHome() {
-        scroller.scrollTo('home-container', {
-          duration: 1000,
-          delay: 100,
-          smooth: 'easeInOutQuart'
-        })
-      }
+  render() {
+    const { location } = this.props;
 
+    const navbarColor =
+      location.pathname === "/signup"
+        ? { background: "black" }
+        : { background: this.state.color, position: "fixed" };
 
-     
-      
-      render() {
+    return (
+      <Navigation dark expand="md" style={navbarColor} id="nav">
+        <NavbarBrand href="/" style={{ padding: "0 20px" }}>
+          <h3>SeeMe</h3>
+        </NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
 
-        const { location } = this.props;
-        const navbarColor = location.pathname === '/signup' ? {background: 'black'} : { background: 'transparent', position: 'fixed' }
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto">
+            <NavItem>
+              <NavLink
+                tag={Link}
+                exact
+                to="/"
+                onClick={() => this.scrollToHome()}
+                className="inactive"
+                activeClassName="active"
+              >
+                <h5>Home</h5>
+              </NavLink>
+            </NavItem>
 
+            <NavItem>
+              <NavLink
+                tag={Link}
+                to="/about"
+                onClick={() => this.scrollToAbout()}
+                className="inactive"
+                activeClassName="active"
+              >
+                <h5>About</h5>
+              </NavLink>
+            </NavItem>
 
-        return (
-            <Navigation dark expand="md" style={navbarColor}>
-                <NavbarBrand href="/" style={{padding: '0 20px'}}><h3>SeeMe</h3></NavbarBrand>
-                <NavbarToggler onClick={this.toggle} />
-
-                <Collapse isOpen={this.state.isOpen} navbar>
-
-
-                    <Nav className="ml-auto">
-
-
-
-                        <NavItem>
-                            <NavLink tag={Link} exact to="/" onClick={() => this.scrollToHome()} className="inactive" activeClassName="active">
-                                <h5>Home</h5>
-                            </NavLink>
-
-                        </NavItem>
-
-
-                        <NavItem>
-                            <NavLink tag={Link} to="/about" onClick={() => this.scrollToAbout()} className="inactive" activeClassName="active">
-                                <h5>About</h5>
-                            </NavLink>
-                        </NavItem>
-
-
-
-                        <NavItem>
-                            <NavLink tag={Link} to="/signup" className="inactive" activeClassName="active">
-                                <h5>Signup</h5>
-                            </NavLink>
-                        </NavItem>
-
-
-                    </Nav>
-                </Collapse>
-            </Navigation>
-
-        )
-    }
+            <NavItem>
+              <NavLink
+                tag={Link}
+                to="/signup"
+                className="inactive"
+                activeClassName="active"
+              >
+                <h5>Signup</h5>
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navigation>
+    );
+  }
 }
 
 export default withRouter(Navbar);
