5a53cab8508df0d78ae089a83f110254b33063ca
Jas <c.jasminher@gmail.com>
2020-03-31 21:53:28 -0400

    refactor: Added props to handle content

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -5,10 +5,6 @@ import '../css/Signup.css'
 
 const signup = [{ title: 'Full Name', id: 'name', placeholder: 'Name' }, { title: 'Email', id: 'email', placeholder: 'Email address', type: 'email' }, { title: 'Username', id: 'username', placeholder: "Username" }, { title: 'Password', id: 'password', placeholder: '*********', type: 'password' }, { title: 'Repeat Password', id: 'repassword', placeholder: '*********' }];
 
-const signin = [{ title: 'Username', id: 'username', placeholder: "Username" }, { title: 'Password', id: 'password', placeholder: '******', type: 'password' }];
-
-
-
 function Signup() {
   return (
 
@@ -28,7 +24,7 @@ function Signup() {
         <Col lg="4" style={{padding: '40px 40px 10px 40px'}} >
 
           {/* add condition to render form depending on if it is signup or sign */}
-          <Form title="Sign Up" labels={signup} />
+          <Form title="Sign Up" labels={signup} button="Sign Up" to="/signin" question="Already have an account?" linkto="Sign In" />
         </Col>
 
       </Row>
