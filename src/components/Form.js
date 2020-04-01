21f18e16381072c1b178980a0d4ead8485acd7f5
Jas <c.jasminher@gmail.com>
2020-03-31 21:51:56 -0400

    refactor: Removed row containing text with user agreement, and added props to handle conditional rendering for form content

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,55 +1,59 @@
-import React from 'react';
-import { Form as SignupForm, FormGroup, Label, Input, Row, Col } from 'reactstrap';
-import '../css/Form.css'
-
-
+import React from "react";
+import {
+  Form as SignupForm,
+  FormGroup,
+  Label,
+  Input,
+  Row,
+  Col
+} from "reactstrap";
+import "../css/Form.css";
+import { Link } from "react-router-dom";
 
 class Form extends React.Component {
+  renderLabels(labels) {
+    return (
+      /* We mapped through the labels that are passed in props, so we can  dynamically create labels */
+      labels.map(i => (
+        <FormGroup row>
+          <Label className="form-label">{i.title}</Label>
 
-    renderLabels(labels) {
+          <Input
+            className="form-input"
+            type={i.type ? i.type : "text"}
+            id={i.id}
+            placeholder={i.placeholder}
+          />
+          <span class="focus-border"></span>
+        </FormGroup>
+      ))
+    );
+  }
 
-        return (
-            /* We mapped through the labels that are passed in props, so we can  dynamically create labels */
-            labels.map(i =>
+  render() {
+    return (
+      <SignupForm>
+        <h1 className="form-title"> {this.props.title}</h1>
 
-                <FormGroup row>
-                    <Label className="form-label">{i.title}</Label>
+        {this.renderLabels(this.props.labels)}
 
-                    <Input className="form-input" type={i.type ? i.type : "text"} id={i.id} placeholder={i.placeholder} />
-                    <span class="focus-border"></span>
-                </FormGroup>
-            )
-        )
+        <Row style={{ justifyContent: "center" }}>
+          <button id="signup">
+            {this.props.button}
+            <i
+              className="fas fa-long-arrow-alt-right"
+              style={{ paddingLeft: "5px" }}
+            ></i>
+          </button>
+        </Row>
 
-    }
-
-
-    render() {
-        return (
-
-            <SignupForm>
-                <h1 className="form-title"> {this.props.title}</h1> 
-
-                {this.renderLabels(this.props.labels)}
-
-                <Row>
-
-                    <FormGroup check>
-                        <Label check>
-                            <Input type="checkbox" />
-                            I agree to the Terms of User
-                    </Label>
-                    </FormGroup>
-                </Row>
-
-                <Row style={{justifyContent: 'center'}}>
-                    <button id="signup"> Sign Up</button>
-                    <button id="signin"> Sign In <i className="fas fa-long-arrow-alt-right" style={{paddingLeft: '5px'}}></i> </button>
-                </Row>
-
-            </SignupForm>
-        );
-    }
+        <Row>
+          <p>{this.props.question}</p>
+          <Link to={this.props.to}>{this.props.linkto}</Link>
+        </Row>
+      </SignupForm>
+    );
+  }
 }
 
 export default Form;
