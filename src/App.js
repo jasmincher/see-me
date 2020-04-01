d0acd0b918b80a322b124687100241e046b2efb7
Jas <c.jasminher@gmail.com>
2020-03-31 21:50:12 -0400

    refactor: Added route for signin page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Navbar from './components/Navbar'
 import Home from './pages/Home';
 import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+
 import NotFound from './pages/NotFound'
 import { Route, Switch} from 'react-router-dom';
 
@@ -20,6 +22,7 @@ function App() {
       
        <Route exact path={["/", "/about"]} component={Home} />
        <Route path="/signup" component={Signup} />
+       <Route path="/signin" component={Signin} />
        <Route component={NotFound} />
 
      </Switch>
